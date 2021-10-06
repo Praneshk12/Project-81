7234e83fd6c4552b8c079fcdfddc3d719cf68484
Praneshk12 <77246096+Praneshk12@users.noreply.github.com>
2021-10-05 17:46:42 -0700

    Update DrawerNavigator.js

--- DrawerNavigator.js
+++ DrawerNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import TabNavigator from "./TabNavigator";
-import Profile from "../screens/Profile";
+import Profile from "./Profile";
 
 const Drawer = createDrawerNavigator();
 
