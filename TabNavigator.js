f34ac73139ac6c7dab55acd30d565f61699ede82
Praneshk12 <77246096+Praneshk12@users.noreply.github.com>
2021-10-05 17:47:28 -0700

    Update TabNavigator.js

--- TabNavigator.js
+++ TabNavigator.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Feed from "../screens/Feed";
-import CreateStory from "../screens/CreateStory";
+import Feed from "./Feed";
+import CreateStory from "./CreateStory";
 
 const Tab = createBottomTabNavigator();
 
